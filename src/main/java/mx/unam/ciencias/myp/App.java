3b602599df2dc4d3882b4f4a48a9d7008eb2dbc2
Alvaro Ramirez <alvaro@ciencias.unam.mx>
2024-10-24 16:05:31 -0600

    cambio en main()

--- src/main/java/mx/unam/ciencias/myp/App.java
+++ src/main/java/mx/unam/ciencias/myp/App.java
@@ -5,6 +5,6 @@ package mx.unam.ciencias.myp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola a todos");
     }
 }
